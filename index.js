07e5b3d93c93e58a48ee415bb2dce82cbb38ef89
Doug Wright <mrdougwright@gmail.com>
2018-02-19 21:19:10 -0800

    add state and actions and api call

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-// index.js
-console.log('hello parcel')
-
 import { h, app } from 'hyperapp'
+import debounce from 'debounce-promise'
+import './styles/main.sass'
 
-const view = () =>
-  <div>
-    hello hyperapp
-  </div>
+const state = {
+  username: '',
+  userData: null,
+}
 
-app({}, {}, view, document.body)
+// redux-like actions
+const actions = {
+  updateUserName: (username) => (state, actions) => {
+    getUserData(username).then(actions.setUserData)
+    return { username }
+  },
+  setUserData: userData => state => ({ userData })
+}
+
+
+// api call
+const getUserDataFn = username => {
+  return fetch(`https://api.github.com/users/${username}`)
+           .then(res => res.json())
+}
+const getUserData = debounce(getUserDataFn, 700)
+
+
+// view
+const view = (state, actions) =>
+  <main>
+    <div>Search github users:</div>
+    <input
+      type='text'
+      className='searchInput'
+      value={state.username}
+      oninput={e => actions.updateUserName(e.target.value)}
+    />
+    <br />
+    <div className='userCard'>
+      {state.userData ? (
+        <div>
+          <img class='userCard__img' src={state.userData.avatar_url} />
+          <div class='userCard__name'>{state.userData.name}</div>
+          <div class='userCard__location'>{state.userData.location}</div>
+        </div>
+      ) : (
+        <div>👆 search 'em</div>
+      )}
+    </div>
+  </main>
+
+
 // app(state, actions, view, DOM el to attach to)
+app(state, actions, view, document.body)
---
+++ readme.md
@@ -0,0 +1,3 @@
+Basic setup:
+$ mkdir hyperparcel && cd $_
+$ npm init -y && npm i hyperapp parcel-bundler babel-plugin-transform-react-jsx babel-preset-env
